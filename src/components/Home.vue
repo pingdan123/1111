5e7bbcf0f9971a01cef97f7f1066129fb44d9021
孙起嫦 <5647183+sun_qi_chang@user.noreply.gitee.com>
2020-01-24 21:05:43 +0800

    add files

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div>aa
 <el-button type="info" @click="logout">退出</el-button>
     </div>
 </template>
@@ -11,6 +11,6 @@ export default {
 }}
 }
 </script>
-<style >
+<style>
 
 </style>
